c56214bb70078547a9b9aaae5ffb504dd6ccde91
wyf32268886 <1520246115@qq.comxo>
2021-03-20 12:15:05 +0800

    init

---
+++ aiu-front/src/components/common/NavMenu.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="currentPath"
+      router
+      mode="horizontal"
+      background-color="white"
+      text-color="#222"
+      active-text-color="red"
+      style="min-width: 1300px">
+      <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
+        {{ item.navItem }}
+      </el-menu-item>
+      <span style="position: absolute;padding-top: 20px;right: 43%;font-size: 20px;font-weight: bold">WHU Animals</span>
+      <el-input
+        placeholder="快速搜索..."
+        prefix-icon="el-icon-search"
+        size="medium"
+        style="width: 300px;position:absolute;margin-top: 12px;right: 18%"
+        v-model="keywords">
+      </el-input>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavMenu',
+    data () {
+      return {
+        navList: [
+          {name: '/index', navItem: '首页'},
+          {name: '/jotter', navItem: '留言区'},
+          {name: '/library', navItem: '动物信息'},
+          {name: '/login', navItem: '个人中心'}
+        ],
+        keywords: ''
+      }
+    },
+    computed: {
+      hoverBackground () {
+        return '#ffd04b'
+      },
+      currentPath () {
+        var x = this.$route.path.indexOf('/', 1)
+        if (x !== -1) {
+          return this.$route.path.substring(0, x)
+        } else {
+          return this.$route.path
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  a{
+    text-decoration: none;
+  }
+
+  span {
+    pointer-events: none;
+  }
+
+</style>
---
+++ aiu-front/src/components/library/LibraryIndex.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-container>
+    <el-aside style="width: 200px;margin-top: 20px">
+      <switch/>
+      <SideMenu @indexSelect="listByArea" ref="sideMenu"></SideMenu>
+      <!--侧边菜单-->
+    </el-aside>
+    <el-main>
+      <!--动物展示-->
+      <Books class="animal-show" ref="books"/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import SideMenu from './SideMenu'
+import Books from './Books'
+export default {
+  name: 'LibraryIndex',
+  components: {SideMenu, Books},
+  methods: {
+    listByArea () {
+      var _this = this
+      var cid = this.$refs.sideMenu.cid
+      var url = 'area/' + cid + '/animal'
+      this.$axios.get(url).then(resp => {
+        if (resp && resp.status === 200) {
+          _this.$refs.animalshow.animal = resp.data
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.animal-show {
+  width: 990px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
